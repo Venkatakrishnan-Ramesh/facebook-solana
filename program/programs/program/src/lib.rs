ae434fe0dd23251970fa5594324b4c1b3c32b56b
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2022-12-17 05:12:37 +0530

    Some minor file changes

--- program/programs/program/src/lib.rs
+++ program/programs/program/src/lib.rs
@@ -13,3 +13,17 @@ pub mod program {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+// Post account Structure 
+//  Simple struct so defined  directly 
+#[account] //
+pub struct PostAccount{
+    pub authority : Pubkey , // who is the person who posted them
+    pub text : String ,
+    pub poster_name : String,
+    pub poster_url : String,
+    pub comment_count : u64,
+    pub index :u64,
+    pub post_time :i64,
+
+}
