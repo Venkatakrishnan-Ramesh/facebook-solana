7b53925cd366302a10998754af51d825453b4cfc
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2022-12-17 11:39:20 +0530

    size were defined for variables

--- program/programs/program/src/lib.rs
+++ program/programs/program/src/lib.rs
@@ -2,6 +2,10 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+const TEXT_LENGTH :usize =1024; // the no of character in a post
+const USER_NAME_LENGTH : usize =100; // size of username
+const USER_URL_LENGTH : usize = 255; // length of the profile img url
+
 #[program]
 pub mod program {
     use super::*;
@@ -39,10 +43,27 @@ pub struct StateAccount{
 
 }
 
-#[derive(Accounts)] {
+#[derive(Accounts)] 
+    pub struct CreatePost<'info>{
+
+    #[account(mut,seeds=[b"state".as_ref()],bump)]    
     pub state : Account<'info,StateAccount>, // letting the Account know that it is a state account
-    
+
+    // Authenticate  Post Account to check the validation of the post
+    #[account(
+        init,
+        seeds=[b"post".as_ref(),state.post_count.to_be_bytes().as_ref()], // seed here is used as a unique key in analogy to database
+        bump,
+        payer=authority
+        // Space can be expecitely defined using low-level languages such as rust
+        space=size_of::<PostAccount>() + USER_URL_LENGTH + TEXT_LENGTH + USER_NAME_LENGTH;
+
+    )]
+    pub post:Account<'info,PostAccount>,
 }
+
+
+
 
 
 
